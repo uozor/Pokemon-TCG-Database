212ccbfd790cd3c55bd139d695ba2ad9e0b3e433
Ukpabia Ozor <ozorcard@gmail.com>
2021-10-05 15:39:25 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+const poke = document.body.querySelector(".poke").value;
+const body = document.body
+/*export default {
+  host: 'https://api.pokemontcg.io/v2'
+}  
+import configuration from './configure';
+import queryBuilder from './queryBuilder';
+
+const configure = ({apiKey}) => {
+    configuration.apiKey = apiKey;
+};
+
+export default {
+    configure,
+    card: queryBuilder('cards'),
+    set: queryBuilder('sets'),
+    type: queryBuilder('types'),
+    subtype: queryBuilder('subtypes'),
+    rarity: queryBuilder('rarities'),
+    supertype: queryBuilder('supertypes')
+} 
+
+pokemon.card.find('base1-4')
+.then(card => {
+    console.log(card.name) // "Charizard"
+
+})
+*/
+document.querySelector('#searchPoke').addEventListener('click', () => {
+  fetch('https://api.pokemontcg.io/v2/cards')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const poke = document.querySelector(".poke").value;
+      //document.querySelector(".poke").value;
+    
+      console.log(poke)
+      data.data.forEach(card => {
+        if (poke.toLowerCase() == card.name.toLowerCase()) {
+          let option = document.createElement('img');
+          //option.textContent = `${card.name} ${card.id}`;
+          option.src = card.images.small;
+          option.classList.add('gen');
+          body.append(option);
+          //sel.classList.remove('hide');
+          img.src = option.value;
+          option.addEventListener('click', function () {
+            img.src = option.value;
+            console.log(img.src);
+          });
+        }
+               
+      });
+    });
+});//catch ((err) => console.log(err));
